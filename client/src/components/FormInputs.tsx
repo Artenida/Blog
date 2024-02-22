5dc9184823f95e9f1b0abd88dfac472fa64fe5d0
Artenida <artenidadaci@gmail.com>
2024-02-22 17:48:50 +0100

    profile page UI

--- client/src/components/FormInputs.tsx
+++ client/src/components/FormInputs.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 
 interface FormInputProps {
   id: string;
-  type: string;
+  type?: string;
   label: string;
   placeholder: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  icon: React.ReactNode; // New prop for the icon
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  icon?: React.ReactNode; 
+  defaultValue?: string | number | undefined; 
 }
 
 const FormInputs: React.FC<FormInputProps> = ({ id, label, placeholder, type, icon, onChange }) => {
   return (
     <div className="mt-3">
-      <div className="flex items-center align-center gap-1">
+      <div className="flex items-center align-center gap-1 pl-1">
       {icon}
-        <label htmlFor={id} className="font-semibold text-custom-color3">
+        <label htmlFor={id} className="font-semibold text-custom-color3 pl-1">
           {label}
         </label>
       </div>
@@ -23,7 +24,7 @@ const FormInputs: React.FC<FormInputProps> = ({ id, label, placeholder, type, ic
         type={type}
         placeholder={placeholder}
         id={id}
-        className="mt-2 border border-custom-color2 px-3 py-2 rounded-md focus:outline-none focus:border-custom-color3 w-[350px]"
+        className="mt-2 border border-custom-color2 px-3 py-2 rounded-md focus:outline-none focus:border-custom-color3 w-full"
         onChange={onChange}
       />
     </div>
--- client/src/pages/Blog/MyAccount.tsx
+++ client/src/pages/Blog/MyAccount.tsx
@@ -1,9 +1,16 @@
 import Sidebar from "../../components/Sidebar";
 import background from "../../assets/about1.avif";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../store/user/userSlice";
+import profile from "../../assets/posts/profile.webp";
+import FormInputs from "../../components/FormInputs";
+import { MediumButton } from "../../components/ButtonComponent";
 
 const MyAccount = () => {
+  const { currentUser } = useSelector(selectUser);
   return (
-    <div className="flex flex-col md:flex-row"
+    <div
+      className="flex flex-col md:flex-row"
       style={{
         backgroundImage: `url(${background})`,
         backgroundSize: "cover",
@@ -12,7 +19,38 @@ const MyAccount = () => {
       <div className="bg-white opacity-30 inset-0"></div>
       <Sidebar />
 
-<h2>My account</h2>
+      <div className="max-w-lg mx-auto p-3 w-full">
+        <div className="flex flex-col gap-3">
+          <div
+            className="w-32 h-32 self-center cursor-pointer shadow-md 
+          overflow-hidden rounded-full border-4"
+          >
+            {/* <img src={currentUser.user.profile_picture} alt="" /> */}
+            <img src={profile} alt="" className="rounded-full w-32 h-32" />
+          </div>
+          <FormInputs
+            id="username"
+            label="Username"
+            placeholder="Username"
+            defaultValue={currentUser.username}
+          />
+          <FormInputs
+            id="email"
+            label="Email"
+            placeholder="Email"
+            defaultValue={currentUser.email}
+          />
+          <FormInputs id="password" label="Password" placeholder="Password" />
+          <FormInputs id="bio" label="Bio" placeholder="Bio" />
+
+          <MediumButton>Update</MediumButton>
+
+          <div className="text-red-500 flex justify-between mt-5">
+            <span className="cursor-pointer">Delete Account</span>
+            <span className="cursor-pointer">Sign Out</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
