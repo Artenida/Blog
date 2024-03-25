16b8ddde7c3c13fe9b3ac086573ac275c0f20683
Artenida <artenidadaci@gmail.com>
2024-03-25 11:21:32 +0100

    UI changes

--- client/src/components/pagination/PaginationButtons.tsx
+++ client/src/components/pagination/PaginationButtons.tsx
@@ -1,7 +1,7 @@
-import { spawn } from 'child_process';
-import React from 'react';
-import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
-import ReactPaginate from 'react-paginate';
+import { spawn } from "child_process";
+import React from "react";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import ReactPaginate from "react-paginate";
 
 interface PaginationButtonsProps {
   totalItems: number;
@@ -14,32 +14,46 @@ const PaginationButtons: React.FC<PaginationButtonsProps> = ({
   totalItems,
   itemsPerPage,
   currentPage,
-  onPageChange
+  onPageChange,
 }) => {
   const pageCount = Math.ceil(totalItems / itemsPerPage);
 
+  const getPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 0; i < pageCount; i++) {
+      pageNumbers.push(i);
+    }
+    return pageNumbers;
+  };
+
+  const pageNumbers = getPageNumbers();
+
+  const handleClick = (pageNumber: any) => {
+    onPageChange(pageNumber);
+  };
+
   return (
     <ReactPaginate
-    breakLabel = {
-      <span className='mr-4'>...</span>
-    }
-    nextLabel = {
-      <span className='w-10 h-10 flex items-center justify-center bg-custom-color2 rounded-md'>
-        <FaAngleRight className='text-custom-color3' />
-      </span>
-    }
+      breakLabel={<span className="mr-4">...</span>}
+      nextLabel={
+        <span className="w-10 h-10 flex items-center justify-center bg-custom-color2 rounded-md">
+          <FaAngleRight className="text-custom-color3" />
+        </span>
+      }
       pageCount={pageCount}
-      pageRangeDisplayed={3}
-      // marginPagesDisplayed={2}
-      previousLabel = {
-        <span className='w-10 h-10 flex items-center justify-center bg-custom-color2 rounded-md mr-4'>
-          <FaAngleLeft className='text-custom-color3'/>
+      pageRangeDisplayed={1}
+      marginPagesDisplayed={2}
+      previousLabel={
+        <span className="w-10 h-10 flex items-center justify-center bg-custom-color2 rounded-md mr-4">
+          <FaAngleLeft className="text-custom-color3" />
         </span>
       }
       onPageChange={({ selected }) => onPageChange(selected)}
-      containerClassName='flex items-center justify-center mt-12'
-  pageClassName={'inline-flex items-center justify-center border border-solid border-custom-color2 hover:bg-custom-color2 w-10 h-10 rounded-md mr-4 cursor-pointer'}
-  activeClassName='bg-custom-color3 text-custom-color1'
+      containerClassName="flex items-center justify-center mt-12"
+      pageClassName={
+        "inline-flex items-center justify-center border border-solid border-custom-color2 hover:bg-custom-color2 w-10 h-10 rounded-md mr-4 cursor-pointer"
+      }
+      activeClassName="bg-custom-color3 text-custom-color1"
     />
   );
 };
