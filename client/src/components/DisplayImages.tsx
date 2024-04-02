e3ecffd44dffbe46a4c978a46d1281f7bc3ee98a
Artenida <artenidadaci@gmail.com>
2024-04-02 16:37:34 +0200

    UI fixes

--- client/src/components/DisplayImages.tsx
+++ client/src/components/DisplayImages.tsx
@@ -41,38 +41,33 @@ const DisplayImages: React.FC<BlogCardProps> = ({ posts }) => {
   };
 
   return (
-    <div className="flex justify-center gap-3">
-      <div className="flex gap-4">
-        {posts.map((post) => (
-          <div key={post.post_id}>
-            {post.images &&
-              post.images.length > 0 &&
-              post.images.map((image, index: number) => (
-                <div key={index} className="mt-5">
-                  <Link to={`/blog/${post.post_id}`} key={index}>
-                    <img
-                      src={`http://localhost:5000/${image.url.replace(
-                        /\\/g,
-                        "/"
-                      )}`}
-                      alt="blogPicture"
-                      className="w-full h-64 rounded-lg"
-                      onClick={() => handleClick(image.url)}
-                    />
-                  </Link>
-                  </div>
-              ))}
-          </div>
-        ))}
-      </div>
-
-      <div>
+    <div className="flex justify-center flex-col gap-3 items-center">
+        <div>
         {mainImage && (
           <img
-            className="rounded-xl w-[800px] h-[600px]"
+            className="rounded-xl w-[500px] h-[500px]"
             src={`http://localhost:5000/${mainImage.replace(/\\/g, "/")}`}
             alt="Main Image"
           />
+        )}
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        {posts.map((post) =>
+          post.images.map((image, index: number) => (
+            <div key={index} className="mt-5">
+              <Link to={`/blog/${post.post_id}`} key={index}>
+                <img
+                  src={`http://localhost:5000/${image.url.replace(
+                    /\\/g,
+                    "/"
+                  )}`}
+                  alt="blogPicture"
+                  className="w-44 h-44 rounded-lg"
+                  onClick={() => handleClick(image.url)}
+                />
+              </Link>
+            </div>
+          ))
         )}
       </div>
     </div>
