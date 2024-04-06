0cd9d8aac0934ad516c0f676b9f4703615be894b
Artenida <artenidadaci@gmail.com>
2024-04-07 01:45:08 +0200

    interface updates

--- client/src/components/Blog/EmptyPage.tsx
+++ client/src/components/Blog/EmptyPage.tsx
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom";
 
 const EmptyPage = () => {
   return (
-    <div className="flex justify-between align-center p-16 px-56 bg-custom-color2 h-screen">
-      <div>
-        <img className="h-[500px]" src={emptyPage} alt="Error" />
+    <div className="flex flex-col md:flex-row justify-center items-center p-5 md:p-10 bg-custom-color2 min-h-screen">
+      <div className="md:mr-12 md:w-1/2">
+        <img className="h-auto md:h-full max-w-full" src={emptyPage} alt="Error" />
       </div>
-      <div className="pt-32">
-        <h4 className="font-bold text-[40px]">
-          Oops! Your post list is empty.
-        </h4>
-        <h5 className="text-[20px]">
-          Looks like there's a blank canvas waiting for your adventures!
-        </h5>
-        <h5 className="text-[20px]">
-          Why not share your stories, memories, or insights with us?
-        </h5>
-        <div className="py-3 mr-56 mt-8 border-2 border-custom-color3 bg-blue-300 rounded-lg text-center text-[20px]">
-          <Link to="/createPost">Create Post</Link>
+      <div className="pt-8 md:pt-0 md:w-1/2">
+        <div className="border border-custom-color3 p-8 rounded-lg">
+          <h4 className="font-bold text-4xl md:text-5xl mb-4">
+            Oops! Your post list is empty.
+          </h4>
+          <h5 className="text-lg md:text-xl mb-4">
+            Looks like there's a blank canvas waiting for your adventures!
+          </h5>
+          <h5 className="text-lg md:text-xl mb-4">
+            Why not share your stories, memories, or insights with us?
+          </h5>
+          <div className="py-3 mt-8 border-2 border-custom-color3 bg-blue-300 rounded-lg text-center text-lg md:text-xl">
+            <Link to="/createPost">Create Post</Link>
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/Blog/Error.tsx
+++ client/src/components/Blog/Error.tsx
@@ -1,29 +1,29 @@
 import error from "../../assets/error3.png";
 import { Link } from "react-router-dom";
 
-interface Error {
-  message1?: String,
-  message2?: String,
-  message3?: String;
-  message4?: String;
-  message5?: String;
+interface ErrorProps {
+  message1?: string;
+  message2?: string;
+  message3?: string;
+  message4?: string;
+  message5?: string;
 }
 
-const Error: React.FC<Error> = ({message1, message2, message3, message4, message5}) => {
+const Error: React.FC<ErrorProps> = ({ message1, message2, message3, message4, message5 }) => {
   return (
-    <div className="flex justify-between align-center p-10 px-56 bg-custom-color2">
-      <div>
-        <h1 className="font-bold text-[180px]">{message1}</h1>
-        <h3 className="font-semibold text-[60px]">{message2}</h3>
-        <h2 className="font-bold text-[30px]">{message3}</h2>
-        <h5 className="text-[25px]">{message4}</h5>
-        <h5 className="text-[25px]">{message5}</h5>
-        <div className="py-3 mr-56 mt-8 border-2 border-custom-color3 bg-blue-300 rounded-lg text-center text-[20px]">
+    <div className="flex flex-col-reverse md:flex-row justify-center items-center p-5 md:p-10 bg-custom-color2">
+      <div className="md:mr-12 text-center md:text-left">
+        <h1 className="font-bold text-8xl md:text-9xl">{message1}</h1>
+        <h3 className="font-semibold text-4xl md:text-5xl">{message2}</h3>
+        <h2 className="font-bold text-3xl md:text-4xl">{message3}</h2>
+        <h5 className="text-lg md:text-xl">{message4}</h5>
+        <h5 className="text-lg md:text-xl">{message5}</h5>
+        <div className="py-3 mt-8 border-2 border-custom-color3 bg-blue-300 rounded-lg text-center text-xl md:text-2xl">
           <Link to="/">Back to Home</Link>
         </div>
       </div>
-      <div>
-        <img className="h-[500px]" src={error} alt="Error" />
+      <div className="text-center md:text-right">
+        <img className="h-auto md:h-[500px] max-w-full" src={error} alt="Error" />
       </div>
     </div>
   );
